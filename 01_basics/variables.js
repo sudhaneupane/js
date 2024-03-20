84cfc604209e581a73b0d611f83cbe53d6548402
Sudha Neupane <97887004+sudhaneupane@users.noreply.github.com>
2024-03-20 09:55:45 +0000

    variables learned

---
+++ 01_basics/variables.js
@@ -0,0 +1,18 @@
+const accountId=144;
+let accountEmail="sudha.1@gmail.com"
+var accountPassword="12125"
+accountCity="Kathmandu"; //not prefered
+let accountState;
+// accountId=2;  //not allowed
+
+accountEmail="sumi@gmail.com";
+accountPassword="123123"
+accountCity="Sydney"
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
