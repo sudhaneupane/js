973ecf631d84dc8e968eafbd705e46b4bddf6f1d
Sudha <sudaneupane.11@gmail.com>
2024-03-22 15:07:46 +0545

    Array

---
+++ 02_basics/array.js
@@ -0,0 +1,32 @@
+const myArr=[0,1,2,"batman",4,5]  //square bracket
+console.log(myArr[3]);
+
+// const myArr2=new Array(1,5,7,6,75,45)  //parenthesis
+// console.log(myArr2[4]);
+
+
+//array methods
+
+// myArr.push(6)
+// console.log(myArr);
+// myArr.push(7)
+// console.log(myArr);
+// myArr.pop()
+// console.log(myArr);
+
+
+// console.log(myArr.includes(6));
+// console.log(myArr.indexOf(2));
+// console.log(myArr);
+// const newArr3=myArr.join()
+// console.log(newArr3);
+
+console.log("A",myArr);
+const myna1=myArr.slice(1,3)
+console.log(myna1);
+
+console.log("B",myArr);
+const myna2=myArr.splice(1,3)
+console.log(myna2);
+
+console.log("C",myArr);
