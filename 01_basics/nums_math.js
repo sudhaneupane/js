c18dbafca876525b343a869893c60f9fbd037139
Sudha <sudaneupane.11@gmail.com>
2024-03-22 12:20:16 +0545

    maths functions added

--- 01_basics/nums_math.js
+++ 01_basics/nums_math.js
@@ -12,3 +12,19 @@ console.log(otherNumber.toPrecision(5));
 
 const hundreds=1000000
 console.log(hundreds.toLocaleString('en-IN'));
+
+//*****************************Maths***********************
+
+console.log(Math);
+console.log(Math.abs(-7));
+console.log(Math.round(4.7));
+console.log(Math.ceil(4.7));
+console.log(Math.floor(4.7));
+console.log(Math.min(4,8,5,3,9));
+
+console.log(Math.random());
+//console.log(Math.random(1,6));
+
+const min=1;
+const max=6;
+//console.log(Math.floor(Math.random*(max-min+1)+min));
