21598db2b1af3451d33482fba420c321842b6804
Sudha <sudaneupane.11@gmail.com>
2024-03-22 12:48:22 +0545

    dates

---
+++ 01_basics/dates.js
@@ -0,0 +1,16 @@
+// let date=new Date()
+// console.log(date.toDateString());
+// console.log(date.toString());
+// console.log(date.toLocaleString());
+// console.log(typeof date);
+
+// let newdate=new Date("2024-01-21")
+// console.log(newdate.toDateString());
+
+// let myTimeStamp=Date.now()
+// console.log(myTimeStamp);
+
+let newDate=new Date()
+console.log(newDate);
+console.log(newDate.getMonth());
+console.log(newDate.getDay());
