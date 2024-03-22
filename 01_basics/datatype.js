d440dbe1e2080341fb5e32f58095daef76b32b70
Sudha <sudaneupane.11@gmail.com>
2024-03-22 08:37:07 +0545

    datatype summery

---
+++ 01_basics/datatype.js
@@ -0,0 +1,39 @@
+/*Primitives
+
+7 types:String,Number,Boolean,null,undefined,Symbol,BigInt
+
+*/
+
+const score = 100;
+const scoreValue=100.3
+
+const isLoggedIn=false
+const temp=null
+let userEmail=undefined
+
+const id=Symbol('123')
+const anotherId=Symbol('123')
+
+console.log(id);
+console.log(id===anotherId);
+
+const bigNumber=3254788954545784548545n
+console.log(typeof bigNumber);
+
+/*Reference(Non-Primitive)
+
+Array,Objects,Functions
+
+*/
+
+const heroes=["spiderman","wonderwoman","superman","batman"]
+
+let myObj={
+    name: "Sudha",
+    food:"Pizza",
+}
+
+const myFunction = function(){
+    console.log("hello world");
+}
+console.log(typeof id);
