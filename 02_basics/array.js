ff4485419e128492f6d56468582e29c5bbf515dd
Sudha <sudaneupane.11@gmail.com>
2024-03-23 16:31:12 +0545

    array

--- 02_basics/array.js
+++ 02_basics/array.js
@@ -1,29 +1,39 @@
 const myArr=[0,1,2,"batman",4,5]  //square bracket
 console.log(myArr[3]);
 
-// const myArr2=new Array(1,5,7,6,75,45)  //parenthesis
-// console.log(myArr2[4]);
+const myArr2=new Array(1,5,7,6,75,45)  //parenthesis
+console.log(myArr2[4]);
 
 
 //array methods
 
-// myArr.push(6)
-// console.log(myArr);
-// myArr.push(7)
-// console.log(myArr);
-// myArr.pop()
-// console.log(myArr);
+myArr.push(6)
+console.log(myArr);
+myArr.push(7)
+console.log(myArr);
+myArr.pop()
+console.log(myArr);
 
 
-// console.log(myArr.includes(6));
-// console.log(myArr.indexOf(2));
-// console.log(myArr);
-// const newArr3=myArr.join()
-// console.log(newArr3);
+console.log(myArr.includes(6));
+console.log(myArr.indexOf(2));
+console.log(myArr);
+const newArr3=myArr.join()
+console.log(newArr3);
 
 console.log("A",myArr);
 const myna1=myArr.slice(1,3)
 console.log(myna1);
+
+myArr.unshift(12)
+console.log(myArr);
+
+myArr.shift()
+console.log(myArr);
+
+console.log(myArr.includes(5)); 
+
+console.log(myArr.indexOf(3)); 
 
 console.log("B",myArr);
 const myna2=myArr.splice(1,3)
