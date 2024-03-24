6bec28172badfab40e541120f48906f4ca9bdab4
Sudha <sudaneupane.11@gmail.com>
2024-03-24 18:53:08 +0545

    update

--- 03_basics/scope.js
+++ 03_basics/scope.js
@@ -8,8 +8,44 @@ let a =200
 if(true){
     let a =10;
     const b=20
-    console.log("inner",a);
+    //console.log("inner",a);
 }
- console.log(a);
+ // console.log(a);
 // console.log(b);
 // console.log(c);
+
+function one(){
+    const userName="sudha"
+    function two(){
+        const website="youtube"
+        console.log(userName);
+    }
+    // console.log(website);
+
+    two()
+}
+
+// one()
+
+if (true){
+    const userName="makima"
+    if (userName==="makima") {
+        const website="  youtube"
+        console.log(userName+website);
+    }
+    // console.log(website);
+}
+
+// console.log(userName);
+
+//****************************************interesting****************** */
+
+function addOne(num){
+    return num+1
+}
+addOne(4)
+
+const addTwo= function(num){
+    return num +1
+}
+addTwo(8)
