8666065ebce57a576540d4bcb39add4be14840f2
Sudha <sudaneupane.11@gmail.com>
2024-03-26 15:24:27 +0545

    updated IIFE

--- 03_basics/iife.js
+++ 03_basics/iife.js
@@ -1,12 +1,14 @@
-//immediately invoked function expression
+//immediately invoked function expression (IIFE)
 
+
+// parenthesis before function to separate pollution from global scope
 (function db(){
+    //named IIFE
     console.log(`database connected`);
 
 }) ();
 
-(()=>{
-    console.log("sudha");
-}) ()
+((name)=>{
+    console.log(`DB connected twice ${name}`);
+})("Madhu")
 
-// db()
