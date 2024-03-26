1e39e6682121964ae7d085f1cfd039522f9bc8dd
Sudha <sudaneupane.11@gmail.com>
2024-03-26 16:15:15 +0545

    switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,32 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month="March"
+switch (month) {
+    case 1:
+        console.log("January");
+        break; 
+    case 2:
+        console.log("Feburary");
+        break;
+    case "March":
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+    case 2:
+        console.log("May");
+        break;
+
+
+    default:
+        console.log("not a valid month");
+        break;
+}
