cd8bc34cef792e1029340175e62278b4be7b17c4
Sudha <sudaneupane.11@gmail.com>
2024-03-28 10:50:23 +0545

    while,do while loop

---
+++ 05_iterators/while.js
@@ -0,0 +1,19 @@
+let index=0
+while (index<=10) {
+    console.log(`The value of index is ${index}`);
+    index=index+2
+}
+
+let myArr=["Superman","Batman","Spiderman","Deadpool"]
+let count=0
+while (count<myArr.length) {
+    console.log(`Value is ${myArr[count]}`);
+    count++
+}
+
+let score =11
+
+do {
+    console.log(`The score is ${score}`);
+    score++
+} while (score<=10);
