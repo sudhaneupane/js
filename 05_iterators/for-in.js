b4a2c0d45cebbb549bfafef3bfef788223c38842
Sudha <sudaneupane.11@gmail.com>
2024-04-03 15:38:18 +0545

    for in loop

---
+++ 05_iterators/for-in.js
@@ -0,0 +1,27 @@
+const myObj={
+    js:'javascript',
+    cpp:'c++',
+    rb:'ruby',
+    swift:'swift by apple'
+}
+
+for (const key in myObj) {
+//    console.log(`${key} for ${myObj[key]}`);
+}
+
+
+const programming=['js','rb','java','py']
+
+for (const key in programming) {
+//    console.log(key,":-",programming[key]);
+}
+
+
+// const map=new Map()
+// map.set('NEP','Nepal')
+// map.set('FR','France')
+// map.set('IN','India')
+
+// for (const key in map) {
+//    console.log(key);
+// }
